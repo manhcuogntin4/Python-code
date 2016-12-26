7a0e3a82df8bbc9bd8782db2189081354a2ff1fa
cuong-nguyen <manhcuongeic@gmail.com>
2016-12-26 14:41:38 +0100

    Update file readfonts.py

--- readfonts.py
+++ readfonts.py
@@ -1,22 +1,39 @@
 f = open('listfonts.txt', 'r')
 listfonts=[]
+
 for line in f:
+ count_virgule=0
+ count_and=0
  index=line.find(',')
  index1=line.find('&')
  if(index==-1)and(index1==-1):
  	listfonts.append(line)
- position=0
+ position=index
+ first=0
  #print index
  while(index!=-1) and position<len(line) :
- 	temp=line[position:index]
- 	#print temp
+ 	temp=line[first:position]
+ 	print first, position
+ 	print temp
+ 	count_virgule=count_virgule+1
  	listfonts.append(temp)
- 	index=line.find(',',index,len(line))
+ 	first=position+1
+ 	index=line.find(',',position+1,len(line))
+ 	print index
 	if(index!=-1):
-		position+=index
+		position=index
  index=line.find('&')
  if(index!=-1):
  	listfonts.append(line[index+1:])
+ 	count_and=count_and+1
+ if(count_virgule==0)and(count_and==1):
+ 	listfonts.append(line[:index])
+ if(count_virgule==1)and(count_and==0):
+ 	listfonts.append(line[position+1:])
+ if(count_and==1)and(count_virgule!=0):
+ 	listfonts.append(line[position+1:index])
+file_fonts=open('listfontresult.txt','w')
 for i in listfonts:
+	file_fonts.write(i+ "\n")
 	print i
 f.close()
